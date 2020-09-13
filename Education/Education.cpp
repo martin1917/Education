380f4cfd6afa160d3bd14eebdfaa903abe94e7fc
martin1917 <63655028+martin1917@users.noreply.github.com>
2020-09-13 15:22:13 +0300

    Вывод массива(красивый :) )

--- Education/Education.cpp
+++ Education/Education.cpp
@@ -4,5 +4,17 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
+    const int k = 5;
+    int map[k];
+
+    for (int i = 0; i < k; i++)
+    {
+        cin >> map[i];
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+        cout << "map[" << i << "]" << " = " << map[i] << endl;
+    }
 }
 
